1485c4369707c83f71749e4b6f033166befcc749
Jonathan Moraes <arkyebr@gmail.com>
2017-06-19 07:03:08 -0300

    #2 #3 - Criar entrada da Calculadora

--- calculadora.cpp
+++ calculadora.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
+#include <vector>
 
 // Código usado para validar commits que
 // referenciam mais de um requisito
 
+using namespace std;
+
 int main()
 {
+	vector<int> numbers;
+	int number;
+	while(cin >> number and number)
+	{
+		numbers.push_back(number);
+	}
+
 	return 0;
 }
